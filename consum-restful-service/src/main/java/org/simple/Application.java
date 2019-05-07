f5f14895372ebb1ae9d7b8801ead908a99ace1eb
simplewz <13132398914@163.com>
2019-05-07 11:22:25 +0800

    添加使用SpringBoot创建一个应用接收符合RESTful风格接口的数据

---
+++ consum-restful-service/src/main/java/org/simple/Application.java
@@ -0,0 +1,37 @@
+package org.simple;
+
+import org.simple.entity.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+	
+	private static final Logger logger=LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			logger.info(quote.toString());
+		};
+	}
+	
+}
